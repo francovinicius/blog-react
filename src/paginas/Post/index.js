513d205248602331bb35912bf3cc2ac9dfe5292e
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-02-16 14:52:02 -0300

    correção no banner para não aparecer no 404 e finalização do erro 404 juntamente com a finalização do projeto

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,8 +1,10 @@
-import { useParams } from "react-router-dom";
+import { Route, Routes, useParams } from "react-router-dom";
 import posts from "json/posts.json";
 import PostModelo from "componentes/PostModelo";
 import ReactMarkdown from "react-markdown";
 import styles from "./Post.css";
+import NaoEncontrada from "paginas/NaoEncontrada";
+import PaginaPadrao from "componentes/PaginaPadrão";
 
 export default function Post() {
     const parametros = useParams();
@@ -12,20 +14,23 @@ export default function Post() {
     });
 
     if (!post) {
-        return <h1>Post n encontrado</h1>
+        return <NaoEncontrada />
     }
-    
+
     return (
-        <PostModelo
-        fotoCapa={`/assets/posts/${post.id}/capa.png`}
-        titulo={post.titulo}
-        >
-            <div className="post-markdown-container">
-                <ReactMarkdown>
-                    {post.texto}
-                </ReactMarkdown>
-            </div>
-            
-        </PostModelo>
+        <Routes>
+            <Route path="*" element={<PaginaPadrao />}>
+                <Route index element={<PostModelo
+                    fotoCapa={`/assets/posts/${post.id}/capa.png`}
+                    titulo={post.titulo}
+                >
+                    <div className="post-markdown-container">
+                        <ReactMarkdown>
+                            {post.texto}
+                        </ReactMarkdown>
+                    </div>
+                </PostModelo>} />
+            </Route>
+        </Routes>
     )
 }
