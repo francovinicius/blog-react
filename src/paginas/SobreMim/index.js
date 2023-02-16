c68156d1dce2a7999d6d351043ac02468265dd7a
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-02-16 09:59:46 -0300

    atualizando o css do sobre

--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -21,28 +21,28 @@ export default function SobreMim() {
                 alt="Foto do Vinícius com microfone"
                 className={styles.fotoSobreMim}
             />
-            <p>
+            <p className={styles.paragrafo} >
                 Desenvolvedor front-end. Posso desenvolver sites responsivos a partir do zero e transformá-los em experiências web modernas e fáceis de usar.
             </p>
 
-            <p>
+            <p className={styles.paragrafo} >
                 Antes de mergulhar de cabeça no desenvolvimento web, trabalhei em uma empresa onde era responsável pela manutenção dos laboratórios de informática, fui supervisor de operações, ocupei a cadeira de professor de matemática e física em pré vestibulares, e realizei trabalhos voluntários que consistiam no ensino da Matemática de uma forma mais lúdica para comunidades carentes e zona rural. Foi um período de grande crescimento pessoal e profissional, onde adquiri aptidão para encarar situações de extrema pressão. Tenho experiência com resolução de problemas e tomadas de decisão, em supervisionar, traçar metas e estratégias em equipe, entre outras atividades.
             </p>
 
-            <p>
+            <p className={styles.paragrafo} >
                 Já no âmbito da tecnologia, venho aprimorando meus conhecimentos em front-end, prezando sempre em entregar meus projetos com design responsivo começando pelo mobile (Mobile First).
                 Tenho conhecimentos em HTML5, CSS3, JavaScript, e atualmente, tenho me aprofundando nos frameworks React e Bootstrap.
             </p>
 
-            <p>
+            <p className={styles.paragrafo} >
                 Hoje, além de trabalhar com desenvolvimento web, sou responsável por uma equipe que coordena toda a parte de banco de dados da empresa que presto serviços, juntamente com a tratativa e cruzamento de dados do sistema de ponto integrado.
             </p>
 
-            <p>
+            <p className={styles.paragrafo} >
                 Sou formado em Matemática e estou cursando Análise e Desenvolvimento de Sistemas. Divido meu tempo livre em momentos de lazer com a família, igreja, amo tocar violão (montar aquela roda de amigos e fazer um som), gosto de frequentar a academia pela manhã e por último mas não menos importante gosto bastante de fazer trilhas e realizar esportes radicais.
             </p>
 
-            <p>
+            <p className={styles.paragrafo} >
                 Estou sempre bem animado em conhecer pessoas novas, novos ensinamentos e novas culturas! Caso queira saber mais, entre em contato comigo. Um grande abraço!
             </p>
         </PostModelo>
