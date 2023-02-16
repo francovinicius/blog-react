ae25df81619ab32413af99b0726f644b08a6a366
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-02-16 13:50:32 -0300

    finalizando as páginas de post

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom";
 import posts from "json/posts.json";
 import PostModelo from "componentes/PostModelo";
+import ReactMarkdown from "react-markdown";
+import styles from "./Post.css";
 
 export default function Post() {
     const parametros = useParams();
@@ -14,7 +16,12 @@ export default function Post() {
         fotoCapa={`/assets/posts/${post.id}/capa.png`}
         titulo={post.titulo}
         >
-            {post.texto}
+            <div className="post-markdown-container">
+                <ReactMarkdown>
+                    {post.texto}
+                </ReactMarkdown>
+            </div>
+            
         </PostModelo>
     )
 }
